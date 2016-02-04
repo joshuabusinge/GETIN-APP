8bbee5a236b089ade4d433cf12b1b210de11378a
Abdu Ssekalala <assekalala@gmail.com>
2016-02-04 11:14:18 +0300

    form

--- app/src/main/java/ug/getin/app/ui/MappingActivity.java
+++ app/src/main/java/ug/getin/app/ui/MappingActivity.java
@@ -1,5 +1,6 @@
 package ug.getin.app.ui;
 
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -10,9 +11,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
@@ -24,10 +34,19 @@ import ug.getin.app.models.Girl;
 import ug.getin.app.service.ApiClient;
 import ug.getin.app.service.ApiService;
 
-public class MappingActivity extends AppCompatActivity implements Callback<Girl> {
+public class MappingActivity extends AppCompatActivity implements OnClickListener, Callback<Girl> {
     Context context = this;
+    Calendar calendar = Calendar.getInstance();
+
 
     EditText nameField;
+
+    EditText dateField;
+
+    DatePickerDialog.OnDateSetListener date;
+    DatePickerDialog datePickerDialog;
+    SimpleDateFormat dateFormatter;
+
     Spinner maritalStatusSpinner;
     Spinner educationLevelSpinner;
     Button submitBtn;
@@ -43,6 +62,15 @@ public class MappingActivity extends AppCompatActivity implements Callback<Girl>
             "Divorced",
             "Widowed"
     };
+
+    String[] education_levels = {
+            "None",
+            "Primary",
+            "Secondary",
+            "Tertiary",
+    };
+
+    String name, dob, marital_status, education_level;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +88,9 @@ public class MappingActivity extends AppCompatActivity implements Callback<Girl>
 //            }
 //        });
 
+
+
+
         restAdapter = new ApiClient().getRestAdapter();
         service = restAdapter.create(ApiService.class);
 
@@ -67,22 +98,65 @@ public class MappingActivity extends AppCompatActivity implements Callback<Girl>
         progressDialog.setMessage("Loading...");
 
         nameField = (EditText) findViewById(R.id.nameField);
+        dateField = (EditText) findViewById(R.id.dob);
+        maritalStatusSpinner = (Spinner) findViewById(R.id.marital_status);
+        educationLevelSpinner = (Spinner) findViewById(R.id.education_level);
         submitBtn = (Button) findViewById(R.id.submitBtn);
 
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Girl girl = new Girl();
-                girl.setName(nameField.getText().toString());
-                girl.setDate_of_birth("2000-01-01");
-                girl.setMarital_status("Single");
-                girl.setEducation_level("Primary");
 
-                progressDialog.show();
-                service.registerGirl(girl, MappingActivity.this);
+        dateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                dateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+
+                dateField.setText(dateFormatter.format(calendar.getTime()));
             }
-        });
+
+        };
+
+        maritalStatusSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, marital_status_choices));
+        educationLevelSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, education_levels));
+
+        dateField.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == dateField) {
+            Log.e("ER", "dateField cliecke");
+
+            new DatePickerDialog(MappingActivity.this, date, calendar
+                    .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)).show();
+
+        }
+        else if(view == submitBtn) {
+            name = nameField.getText().toString();
+            dob = dateField.getText().toString();
+            marital_status = ((TextView) maritalStatusSpinner.getSelectedView()).getText().toString();
+            education_level = ((TextView) educationLevelSpinner.getSelectedView()).getText().toString();
+
+            Girl girl = new Girl();
+            girl.setName(name);
+            girl.setDate_of_birth(dob);
+            girl.setMarital_status(marital_status);
+            girl.setEducation_level(education_level);
+
+            progressDialog.show();
+            service.registerGirl(girl, MappingActivity.this);
+        }
+    }
+
 
     @Override
     public void success(Girl girl, Response response) {
@@ -92,6 +166,8 @@ public class MappingActivity extends AppCompatActivity implements Callback<Girl>
 
     @Override
     public void failure(RetrofitError error) {
+        progressDialog.dismiss();
 
     }
+
 }
