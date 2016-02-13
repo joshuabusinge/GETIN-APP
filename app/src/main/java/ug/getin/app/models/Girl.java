6afd3dc4d52ce64d4693c343f828d652a339ce18
Abdu Ssekalala <assekalala@gmail.com>
2016-02-13 16:05:18 +0300

    added location and more fields

--- app/src/main/java/ug/getin/app/models/Girl.java
+++ app/src/main/java/ug/getin/app/models/Girl.java
@@ -6,6 +6,114 @@ package ug.getin.app.models;
 public class Girl {
     public String name;
     public String date_of_birth;
+    public int age;
+    public String marital_status;
+    public String education_level;
+    public String emergency_contact;
+
+    public String getEmergency_contact() {
+        return emergency_contact;
+    }
+
+    public void setEmergency_contact(String emergency_contact) {
+        this.emergency_contact = emergency_contact;
+    }
+
+    public String getContact_type() {
+        return contact_type;
+    }
+
+    public void setContact_type(String contact_type) {
+        this.contact_type = contact_type;
+    }
+
+    public String getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public String contact_type;
+    public String contact_number;
+    public String number_of_children;
+
+    public String getNumber_of_children() {
+        return number_of_children;
+    }
+
+    public void setNumber_of_children(String number_of_children) {
+        this.number_of_children = number_of_children;
+    }
+
+    public Boolean getHas_gone_for_anc() {
+        return has_gone_for_anc;
+    }
+
+    public void setHas_gone_for_anc(Boolean has_gone_for_anc) {
+        this.has_gone_for_anc = has_gone_for_anc;
+    }
+
+    public String getPrefered_language() {
+        return prefered_language;
+    }
+
+    public void setPrefered_language(String prefered_language) {
+        this.prefered_language = prefered_language;
+    }
+
+    public String getVillage() {
+        return village;
+    }
+
+    public void setVillage(String village) {
+        this.village = village;
+    }
+
+    public String getParish() {
+        return parish;
+    }
+
+    public void setParish(String parish) {
+        this.parish = parish;
+    }
+
+    public String getSubcounty() {
+        return subcounty;
+    }
+
+    public void setSubcounty(String subcounty) {
+        this.subcounty = subcounty;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Boolean has_gone_for_anc;
+    public String prefered_language;
+    public String village;
+    public String parish;
+    public String subcounty;
+    public double latitude;
+    public double longitude;
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
 
     public int getAge() {
         return age;
@@ -15,25 +123,9 @@ public class Girl {
         this.age = age;
     }
 
-    public int age;
-    public String marital_status;
-    public String education_level;
+    public String getDate_of_birth() { return date_of_birth; }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDate_of_birth() {
-        return date_of_birth;
-    }
-
-    public void setDate_of_birth(String date_of_birth) {
-        this.date_of_birth = date_of_birth;
-    }
+    public void setDate_of_birth(String date_of_birth) { this.date_of_birth = date_of_birth; }
 
     public String getMarital_status() {
         return marital_status;
--- app/src/main/java/ug/getin/app/ui/MappingActivity.java
+++ app/src/main/java/ug/getin/app/ui/MappingActivity.java
@@ -1,12 +1,18 @@
 package ug.getin.app.ui;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -34,21 +40,29 @@ import ug.getin.app.models.Girl;
 import ug.getin.app.service.ApiClient;
 import ug.getin.app.service.ApiService;
 
-public class MappingActivity extends AppCompatActivity implements OnClickListener, Callback<Girl> {
+public class MappingActivity extends AppCompatActivity implements OnClickListener, LocationListener, Callback<Girl> {
     Context context = this;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    TextView txtLat;
+    String lat;
+    String provider;
+    double latitude, longitude;
+    boolean gps_enabled, network_enabled;
     Calendar calendar = Calendar.getInstance();
 
 
-    EditText nameField;
+    EditText nameField, number_of_children_field, villageField, parishField, subcountyField;
 
-    EditText dateField;
+    EditText dateField, emergency_contact_field, contact_number_field;
 
     DatePickerDialog.OnDateSetListener date;
     DatePickerDialog datePickerDialog;
     SimpleDateFormat dateFormatter;
 
     Spinner maritalStatusSpinner;
-    Spinner educationLevelSpinner;
+    Spinner educationLevelSpinner, languageSpinner, contactTypeSpinner;
     Button submitBtn;
 
     RestAdapter restAdapter;
@@ -70,7 +84,23 @@ public class MappingActivity extends AppCompatActivity implements OnClickListene
             "Tertiary",
     };
 
-    String name, dob, marital_status, education_level;
+    String[] language_options = {
+            "English",
+            "Luganda",
+
+    };
+
+    String[] contact_types = {
+            "Parent",
+            "Guardian",
+            "Spouse",
+            "Sibling",
+
+    };
+
+
+    String name, dob, marital_status, education_level, number_of_children, village, parish, subcounty;
+    String prefered_language, emergency_contact, contact_type, contact_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,16 +108,20 @@ public class MappingActivity extends AppCompatActivity implements OnClickListene
         setContentView(R.layout.activity_mapping);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
 
 
@@ -101,6 +135,16 @@ public class MappingActivity extends AppCompatActivity implements OnClickListene
         dateField = (EditText) findViewById(R.id.dob);
         maritalStatusSpinner = (Spinner) findViewById(R.id.marital_status);
         educationLevelSpinner = (Spinner) findViewById(R.id.education_level);
+        emergency_contact_field = (EditText) findViewById(R.id.emergency_contact);
+        contactTypeSpinner = (Spinner) findViewById(R.id.contact_type);
+        contact_number_field = (EditText) findViewById(R.id.contact_number);
+        languageSpinner = (Spinner) findViewById(R.id.prefered_language);
+        number_of_children_field = (EditText) findViewById(R.id.number_of_children);
+        villageField = (EditText) findViewById(R.id.village);
+        parishField = (EditText) findViewById(R.id.parish);
+        subcountyField = (EditText) findViewById(R.id.subcounty);
+
+
         submitBtn = (Button) findViewById(R.id.submitBtn);
 
 
@@ -124,6 +168,8 @@ public class MappingActivity extends AppCompatActivity implements OnClickListene
 
         maritalStatusSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, marital_status_choices));
         educationLevelSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, education_levels));
+        languageSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, language_options));
+        contactTypeSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, contact_types));
 
         dateField.setOnClickListener(this);
         submitBtn.setOnClickListener(this);
@@ -141,16 +187,33 @@ public class MappingActivity extends AppCompatActivity implements OnClickListene
 
         }
         else if(view == submitBtn) {
+
             name = nameField.getText().toString();
             dob = dateField.getText().toString();
             marital_status = ((TextView) maritalStatusSpinner.getSelectedView()).getText().toString();
             education_level = ((TextView) educationLevelSpinner.getSelectedView()).getText().toString();
+            emergency_contact = emergency_contact_field.getText().toString();
+            contact_type = (( TextView ) contactTypeSpinner.getSelectedView()).getText().toString();
+            contact_number = contact_number_field.getText().toString();
+            number_of_children = number_of_children_field.getText().toString();
+            prefered_language = ((TextView) languageSpinner.getSelectedView()).getText().toString();
+            village = villageField.getText().toString();
+            parish = parishField.getText().toString();
+            subcounty = subcountyField.getText().toString();
 
             Girl girl = new Girl();
             girl.setName(name);
             girl.setDate_of_birth(dob);
             girl.setMarital_status(marital_status);
             girl.setEducation_level(education_level);
+            girl.setEmergency_contact(emergency_contact);
+            girl.setContact_type(contact_type);
+            girl.setContact_number(contact_number);
+            girl.setNumber_of_children(number_of_children);
+            girl.setPrefered_language(prefered_language);
+            girl.setVillage(village);
+            girl.setParish(parish);
+            girl.setSubcounty(subcounty);
 
             progressDialog.show();
             service.registerGirl(girl, MappingActivity.this);
@@ -170,4 +233,25 @@ public class MappingActivity extends AppCompatActivity implements OnClickListene
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+//        Log.e("LOCATION", "Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Log.d("Latitude","status");
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Log.d("Latitude","disable");
+    }
 }
